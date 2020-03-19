50065d1447c68ca6efce7a277ad6938f99e76800
Алина <alinascarlett@mail.ru>
2020-03-19 12:03:14 +0300

    функция создания запроса

--- js/load.js
+++ js/load.js
@@ -18,11 +18,10 @@
     UPLOAD: 'https://js.dump.academy/keksobooking',
   };
 
-
-  var getAdsData = function (onSuccess, onError) {
+  var createNewRequest = function (method, url, onSuccess, onError) {
     var xhr = new XMLHttpRequest();
+    xhr.timeout = TIMEOUT_IN_MS;
     xhr.responseType = 'json';
-
     xhr.addEventListener('load', function () {
       if (xhr.status === StatusCode.OK) {
         onSuccess(xhr.response);
@@ -30,39 +29,22 @@
         onError('Статус ответа: ' + xhr.status + ' ' + xhr.statusText);
       }
     });
-
     xhr.addEventListener('error', function () {
       onError('Произошла ошибка соединения');
     });
-
     xhr.addEventListener('timeout', function () {
       onError('Запрос не успел выполниться за ' + xhr.timeout + 'мс');
     });
-
-    xhr.timeout = TIMEOUT_IN_MS;
-
-    xhr.open(RequestMethod.GET, ServerUrl.LOAD);
-    xhr.send();
+    xhr.open(method, url);
+    return xhr;
   };
 
-  var sendAdData = function (data, onSuccess, onError) {
-    var xhr = new XMLHttpRequest();
-    xhr.responseType = 'json';
+  var getAdsData = function (onSuccess, onError) {
+    createNewRequest(RequestMethod.GET, ServerUrl.LOAD, onSuccess, onError).send();
+  };
 
-    xhr.addEventListener('load', function () {
-      if (xhr.status === StatusCode.OK) {
-        onSuccess(xhr.response);
-      } else {
-        onError('Статус ответа: ' + xhr.status + ' ' + xhr.statusText);
-      }
-    });
-
-    xhr.addEventListener('error', function () {
-      onError('Произошла ошибка соединения');
-    });
-
-    xhr.open(RequestMethod.POST, ServerUrl.UPLOAD);
-    xhr.send(data);
+  var sendAdData = function (onSuccess, onError, data) {
+    createNewRequest(RequestMethod.POST, ServerUrl.UPLOAD, onSuccess, onError).send(data);
   };
 
   window.load = {
